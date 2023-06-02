fe8e8d2e4df2e87ef868915f20bb5c60e9f6f15b
Shakeel Ahmed <79254929+shakeel2717@users.noreply.github.com>
2023-06-02 16:45:18 +0500

    team image fixed

--- resources/views/user/dashboard/team/index.blade.php
+++ resources/views/user/dashboard/team/index.blade.php
@@ -125,7 +125,7 @@
                     <ul>
                         <li>
                             <a href="#" class="text-white ">
-                                <img class="rounded-square" width="150" src="{{ asset('assets/profile/' . $user->profile) }}"
+                                <img class="rounded-square" width="150" src="{{ asset('assets/profile/default.png') }}"
                                     alt="User Avatar">
                                 <br>
                                 {{ $user->username }}
