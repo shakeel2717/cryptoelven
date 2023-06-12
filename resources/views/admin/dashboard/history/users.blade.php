a2b2584f00dfd9ebf59e8e20ab83cb4ee3899dc9
Shakeel Ahmed <79254929+shakeel2717@users.noreply.github.com>
2023-06-12 19:03:11 +0500

    make pin fixed

--- resources/views/admin/dashboard/history/users.blade.php
+++ resources/views/admin/dashboard/history/users.blade.php
@@ -18,7 +18,6 @@
                             <th class="text-center" style="width: 80px;">#</th>
                                     <th>Name</th>
                                     <th>Username</th>
-                                    <!-- <th>Delete</th> -->
                                     <th>Status</th>
                                     <th>Upliner</th>
                                     <th>Plans</th>
@@ -26,7 +25,6 @@
                                     <th>Action</th>
                                     <th>Balance</th>
                                     <th>ROI Bal</th>
-                                    
                                     <th>ROI</th>
                                     <th>Login</th>
                                     <th>Winner</th>
@@ -36,55 +34,52 @@
                         <tbody>
                             @forelse ($users as $user)
                             <tr>
-                                        <td class="text-center">{{ $loop->iteration }}</td>
-                                        <td class="text-center text-capitalize">{{ $user->name }}</td>
-                                        <td class="text-center text-capitalize">{{ $user->username }}</td>
-                                        <td class="text-center text-capitalize">{{ $user->status }}</td>
-                                        <!-- <td class="text-center text-capitalize"><a href="{{ route('admin.history.user.delete',['id' => $user->id]) }}" class="btn btn-danger text-white">{{ ctse($user->id) }} + {{ myPlanCount($user->id) }}</a></td> -->
-                                        <td class="text-center text-capitalize">{{ $user->refer }}</td>
-                                        <td class="text-center">${{ number_format(myPlanCount($user->id), 2) }}</td>
-                                        <td class="text-center text-capitalize">{{ $user->network == 1 ? 'Yes' : 'No' }}
+                                    <td class="text-center">{{ $loop->iteration }}</td>
+                                    <td class="text-center text-capitalize">{{ $user->name }}</td>
+                                    <td class="text-center text-capitalize">{{ $user->username }}</td>
+                                    <td class="text-center text-capitalize">{{ $user->status }}</td>
+                                    <td class="text-center text-capitalize">{{ $user->refer }}</td>
+                                    <td class="text-center">${{ number_format(myPlanCount($user->id), 2) }}</td>
+                                    <td class="text-center text-capitalize">{{ $user->network == 1 ? 'Yes' : 'No' }}</td>
+                                    <td>
+                                    @if ($user->network == 1)
+                                        <a href="{{ route('admin.history.user.plan.unPin', ['id' => $user->id]) }}"
+                                            class="btn btn-sm btn-success">Make Normal</a>
+                                    @else
+                                        <a href="{{ route('admin.history.user.plan.makePin', ['id' => $user->id]) }}"
+                                                class="btn btn-sm btn-primary">Make PIN</a>
+                                    @endif
+                                    </td>
+                                    <td class="text-center">${{ number_format(balance($user->id), 2) }}</td>
+                                    <td class="text-center">${{ number_format(roiBalance($user->id), 2) }}</td>
+                                   
+                                    <td class="text-center text-capitalize">
+                                    @if ($user->roi == 1)
+                                        <td class="text-center"><a class="btn btn-danger btn-sm"
+                                                href="{{ route('admin.history.users.stop.ROi', ['user' => $user->id]) }}">Stop</a>
                                         </td>
-                                        @if ($user->network == 1)
-                                            <a href="{{ route('admin.history.user.plan.unPin', ['id' => $user->id]) }}"
-                                                class="btn btn-sm btn-success">Make Normal</a>
-                                            </td>
-                                        @else
-                                            <a href="{{ route('admin.history.user.plan.makePin', ['id' => $user->id]) }}"
-                                                    class="btn btn-sm btn-primary">Make PIN</a></td>
-                                        @endif
-                                        
-                                        <td class="text-center">${{ number_format(balance($user->id), 2) }}</td>
-                                        <td class="text-center">${{ number_format(roiBalance($user->id), 2) }}</td>
-                                        
-                                        <td class="text-center text-capitalize">
-                                 @if ($user->roi == 1)
-                                    <td class="text-center"><a class="btn btn-danger btn-sm"
-                                            href="{{ route('admin.history.users.stop.ROi', ['user' => $user->id]) }}">Stop</a>
+                                    @elseif ($user->roi == 0)
+                                        <td class="text-center"><a class="btn btn-success btn-sm"
+                                                href="{{ route('admin.history.users.start.ROi', ['user' => $user->id]) }}">Start</a>
+                                        </td>
+                                    @endif
+                            
+                                    <td>
+                                        <form action="{{ route('admin.login.user') }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="user_id" value="{{ $user->id }}">
+                                            <input class="btn btn-sm btn-dark" type="submit" value="Login" class="login">
+                                        </form>
                                     </td>
-                                @elseif ($user->roi == 0)
-                                    <td class="text-center"><a class="btn btn-success btn-sm"
-                                            href="{{ route('admin.history.users.start.ROi', ['user' => $user->id]) }}">Start</a>
+                                    <td>
+                                        <a class="btn btn-danger btn-sm"
+                                            href="{{ route('admin.winner.user', ['user' => $user->id]) }}">Winner</a>
                                     </td>
-                                @endif
-
-                                
-                                <td>
-                                    <form action="{{ route('admin.login.user') }}" method="POST">
-                                        @csrf
-                                        <input type="hidden" name="user_id" value="{{ $user->id }}">
-                                        <input class="btn btn-sm btn-dark" type="submit" value="Login" class="login">
-                                    </form>
-                                </td>
-                                <td>
-                                    <a class="btn btn-danger btn-sm"
-                                        href="{{ route('admin.winner.user', ['user' => $user->id]) }}">Winner</a>
-                                </td>
-                                <td class="text-center text-capitalize">{{ $user->email }}</td>
-                            </tr>
-                            @empty
-                            <p>No Record Found</p>
-                            @endforelse
+                                    <td class="text-center text-capitalize">{{ $user->email }}</td>
+                                </tr>
+                                @empty
+                                <p>No Record Found</p>
+                                @endforelse
                         </tbody>
                     </table>
                 </div>
