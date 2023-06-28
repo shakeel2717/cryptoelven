6e3632ae316d0e063b3c0a5581dbe1959b7e8954
Shakeel Ahmed <79254929+shakeel2717@users.noreply.github.com>
2023-06-28 14:37:10 +0500

    plans icons added

--- resources/views/user/dashboard/plan/index.blade.php
+++ resources/views/user/dashboard/plan/index.blade.php
@@ -17,9 +17,9 @@
                     <table class="mx-auto">
                         <tbody>
                             <tr>
-                                <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check icon-md text-primary me-2">
-                                        <polyline points="20 6 9 17 4 12"></polyline>
-                                    </svg></td>
+                                <td>
+                                    <img src="{{ asset('assets/plans) }}/{{ $plan->iteration }}" alt="Plan Icon" width="150">
+                                </td>
                                 <td>
                                     <p>Min Withdarw: ${{ number_format(20, 2) }}</p>
                                 </td>
